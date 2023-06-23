f4a5c3e7dc0cfb65df2973de12e34c646d9bb32a
yuuu1010 <ecyu22659@g.nihon-u.co.jp>
2023-06-23 12:12:27 +0900

    first

---
+++ main.js
@@ -0,0 +1,17 @@
+'use strict'
+{
+
+  document.querySelector('.Form-Btn').addEventListener('click', () => {
+    // alert(document.querySelector('textarea').value = '現在、メンテナンス中です！');
+    alert('現在メンテナンス中です！');
+    // document.querySelector('textarea').value = '';
+    // document.querySelector('input').value = '';
+    const textareas = document.querySelectorAll('textarea');
+    const inputs = document.querySelectorAll('input[type="text"], input[type="email"]');
+    
+    textareas.forEach(textarea => textarea.value = '');
+    inputs.forEach(input => input.value = '');
+   
+
+});
+}
